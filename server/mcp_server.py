7fec2f1f53baca958884a2d96f89a0ba9a15bfe9
johndoe775 <johndoe775024@gmail.com>
2025-10-04 13:29:30 +0530

    made edits

--- server/mcp_server.py
+++ server/mcp_server.py
@@ -7,27 +7,23 @@ mcp = FastMCP("My MCP Server")
 
 @mcp.tool
 def greet(name: str) -> str:
+    """A simple tool that greets a person by name."""
     return f"Hello, {name}!"
 
+@mcp.tool
+def resume_rewrite(resume,job_description,personal_info=""):
+    """ use this tool to rewrite resume making it more appealing to the job description """
+    tool_maker(resume,job_description,personal_info).llm_fx(data.keys()[0])
 
 @mcp.tool
-def resume_rewrite(resume, job_description, personal_info=""):
-    tool_maker(resume, job_description, personal_info).llm_fx(data.keys()[0])
-
-
-@mcp.tool
-def cold_mail_info_present(
-    resume,
-    personal_info,
-    job_description="",
-):
-    tool_maker(resume, job_description, personal_info).llm_fx(data.keys()[1])
-
+def cold_mail_info_present(resume,personal_info,job_description="",):
+    """ use this tool to write a cold mail to a recruiter when you have personal info about them """
+    tool_maker(resume,job_description,personal_info).llm_fx(data.keys()[1])
 
 @mcp.tool
-def cold_mail_with_no_info(resume, job_description="", personal_info=""):
-    tool_maker(resume, job_description, personal_info).llm_fx(data.keys()[2])
-
+def cold_mail_with_no_info(resume,job_description="",personal_info=""):
+    """ use this tool to write a cold mail to a recruiter when you have no personal info about them """
+    tool_maker(resume,job_description,personal_info).llm_fx(data.keys()[2])
 
 if __name__ == "__main__":
     mcp.run(transport="http", port=8000)
