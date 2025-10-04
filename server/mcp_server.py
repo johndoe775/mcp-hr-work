6eeb2169a2e90b81e0019f730ee23cf0c76a81ac
johndoe775 <johndoe775024@gmail.com>
2025-10-04 15:03:19 +0530

    recent edits

--- server/mcp_server.py
+++ server/mcp_server.py
@@ -13,17 +13,20 @@ def greet(name: str) -> str:
 @mcp.tool
 def resume_rewrite(resume,job_description,personal_info=""):
     """ use this tool to rewrite resume making it more appealing to the job description """
-    tool_maker(resume,job_description,personal_info).llm_fx(data.keys()[0])
+    response=tool_maker(resume,job_description,personal_info).llm_fx(list(data.keys())[0])
+    return response 
 
 @mcp.tool
 def cold_mail_info_present(resume,personal_info,job_description="",):
     """ use this tool to write a cold mail to a recruiter when you have personal info about them """
-    tool_maker(resume,job_description,personal_info).llm_fx(data.keys()[1])
+    response=tool_maker(resume,job_description,personal_info).llm_fx(list(data.keys())[1])
+    return response
 
 @mcp.tool
 def cold_mail_with_no_info(resume,job_description="",personal_info=""):
     """ use this tool to write a cold mail to a recruiter when you have no personal info about them """
-    tool_maker(resume,job_description,personal_info).llm_fx(data.keys()[2])
+    response=tool_maker(resume,job_description,personal_info).llm_fx(list(data.keys())[2])
+    return response
 
 if __name__ == "__main__":
     mcp.run(transport="http", port=8000)
